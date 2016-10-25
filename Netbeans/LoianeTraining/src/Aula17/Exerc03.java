2f93f8260abe8e0df2875ea9045d79c8206683bf
Josevan Oliveira <josivantarcio@gmail.com>
2016-10-24 21:43:40 -0300

    Alteração Exercicio03 - Aula17

--- Netbeans/LoianeTraining/src/Aula17/Exerc03.java
+++ Netbeans/LoianeTraining/src/Aula17/Exerc03.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Faça um programa que leia e valide as seguintes informações: 
+a. Nome: maior que 3 caracteres; 
+b. Idade: entre 0 e 150; 
+c. Salário: maior que zero; 
+d. Sexo: 'f' ou 'm'; 
+e. Estado Civil: 's', 'c', 'v', 'd'; 
  */
 package Aula17;
 import java.util.Scanner;
@@ -11,6 +14,22 @@ import java.util.Scanner;
  */
 public class Exerc03 {
     public static void main(String[] args) {
-        System.out.println("");
+        Scanner xx = new Scanner(System.in);
+        
+        System.out.print("Digite o nome: ");
+        String nome = xx.nextLine();
+        
+        System.out.print("Digite a idade: ");
+        int idade = xx.nextInt();
+        
+        System.out.print("Salário : ");
+        float salario = xx.nextFloat();
+        
+        System.out.print("Sexo (M-Masculino F-Feminino)");
+        String sexo = xx.next();
+        
+        System.out.print("Estado Civil: S-Solteiro C-Casado V-Viuvo D-Divórciado");
+        String estCivil = xx.next();
+        
     }
 }
