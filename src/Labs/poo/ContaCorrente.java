1f39af70267c5a7b172e1dbaaee048fc566f4b23
Josevan Oliveira <josivantarcio@gmail.com>
2021-08-17 17:51:54 -0300

    Exercicios resolvido
    Feito assim como o enunciado. Simples e sem fugir da explicação inicial.

---
+++ src/Labs/poo/ContaCorrente.java
@@ -0,0 +1,46 @@
+package Labs.poo;
+
+public class ContaCorrente {
+	String numConta;
+	boolean contaEspecial = false;
+	double saldo;
+	double limiteEspecial;
+
+	double sacar(double valorSaque) {
+		double valor = 0;
+		if (contaEspecial) {
+			if (saldo + limiteEspecial > valorSaque) {
+				valor = (saldo + limiteEspecial - valorSaque);
+			} else {
+				System.out.println("!!!! Mesmo com Cheque especial, Saldo Indisponível para o momento");
+			}
+		} else {
+			if (saldo > valorSaque) {
+				valor = (saldo + limiteEspecial - valorSaque);
+			} else {
+				System.out.println("!!!!!  Saldo Indisponível para o momento");
+			}
+		}
+		saldo = valor;
+		return saldo;
+	}
+
+	double depositar(double valorDepositar) {
+		return saldo += valorDepositar;
+	}
+
+	void consultarSaldo() {
+		System.out.println("======= CONTA CORRENTE =======");
+		System.out.println("Conta: " + numConta);
+		System.out.println("Saldo: R$............." + saldo);
+		System.out.println("Cheque Especial: R$..." + limiteEspecial);
+	}
+
+	void usarChequeEspecial() {
+		contaEspecial = true;
+		saldo -= limiteEspecial;
+		System.out.println("Cliente usando o Cheque Especial");
+		
+	}
+
+}
---
+++ src/Labs/poo/Matriz.java
@@ -0,0 +1,38 @@
+package Labs.poo;
+
+import java.util.Scanner;
+
+public class Matriz {
+
+	public static void main(String[] args) {
+		//Exercicio 3 - Aula 25
+		Aluno aluno = new Aluno();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Nome do Aluno: ");
+		aluno.nome = scan.next();
+		
+		System.out.print("Matricula: ");
+		aluno.matricula = scan.next();
+		
+		System.out.print("Curso: ");
+		aluno.curso =  scan.next();
+				 
+		for(int i = 0; i < aluno.disciplinas.length; i++) {
+			System.out.print("Digite a Disciplina nº"+(i+1)+": ");
+			aluno.disciplinas[i] = scan.next();
+			
+		} 
+		
+		for(int i = 0; i < aluno.disciplinas.length; i++) {
+			System.out.println("Digite a Nota da Disciplina "+aluno.disciplinas[i]+": ");
+			aluno.notas[i] = scan.nextDouble();
+		}
+		aluno.analisar();
+		
+		System.out.println("FIM");
+		scan.close();
+
+	}
+
+}
